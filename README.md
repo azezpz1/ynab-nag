1ea7dd64edc56b23e70a3c4e5ea8f482dfe05188
Anthony Panettiere <azezpz1@gmail.com>
2020-08-08 19:50:39 -0500

    Create ynab-nag project

--- README.md
+++
@@ -1,2 +0,0 @@
-# ynab-nag
-A package that emails a user with an alert when they are spending too much
---
+++ tests/test_ynab_nag.py
@@ -0,0 +1,5 @@
+from ynab_nag import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
